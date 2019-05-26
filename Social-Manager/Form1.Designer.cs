9b18c306d717ecfb154b95109727b9fe27793791
coleatkinson1 <40928853+coleatkinson1@users.noreply.github.com>
2019-05-26 13:05:37 +1200

    Add project files.

---
+++ Social-Manager/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Social_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnPanel = new System.Windows.Forms.Panel();
+            this.mPanel = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnPanel
+            // 
+            this.btnPanel.BackColor = System.Drawing.Color.White;
+            this.btnPanel.Location = new System.Drawing.Point(0, 0);
+            this.btnPanel.Name = "btnPanel";
+            this.btnPanel.Size = new System.Drawing.Size(70, 820);
+            this.btnPanel.TabIndex = 0;
+            // 
+            // mPanel
+            // 
+            this.mPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.mPanel.Location = new System.Drawing.Point(67, 0);
+            this.mPanel.Name = "mPanel";
+            this.mPanel.Size = new System.Drawing.Size(1334, 782);
+            this.mPanel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1472, 819);
+            this.Controls.Add(this.mPanel);
+            this.Controls.Add(this.btnPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Social Manager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel btnPanel;
+        private System.Windows.Forms.Panel mPanel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Social-Manager/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Chrome
+using CefSharp;
+using CefSharp.WinForms;
+
+
+namespace Social_Manager
+{
+    
+
+    public partial class Form1 : Form
+    {
+        IDictionary<string, ChromiumWebBrowser> dict = new Dictionary<string, ChromiumWebBrowser>();
+        IDictionary<string, Button> btnDict = new Dictionary<string, Button>();
+        IDictionary<string, Image> btnImage = new Dictionary<string, Image>();
+        string currentWindow = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.SizeChanged += new EventHandler(Form1_Resize);
+
+            dict.Add( "Trello", newBrowser("http://trello.com") );
+            dict.Add( "Facebook", newBrowser("http://facebook.com") );
+            dict.Add( "Gmail", newBrowser("https://accounts.google.com/AccountChooser?service=mail&continue=https://mail.google.com/mail/") );
+
+            
+            btnDict.Add( "Trello", NewButton("Trello", 20) );
+            btnDict.Add( "Facebook", NewButton("Facebook", 90) );
+            btnDict.Add( "Gmail", NewButton("Gmail", 160) );
+
+            
+            btnImage.Add("Trello", Properties.Resources.Trello);
+            btnImage.Add("Facebook", Properties.Resources.Facebook);
+            btnImage.Add("Gmail", Properties.Resources.Gmail);
+
+
+            foreach (KeyValuePair<string, Button> item in btnDict)
+            {
+                btnPanel.Controls.Add(item.Value);
+                System.Diagnostics.Debug.WriteLine(item.Value.Top);
+                item.Value.BackgroundImage = btnImage[item.Key];
+                item.Value.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+
+            //Chrome
+            if (!Cef.IsInitialized)
+            {
+                var settings = new CefSettings();
+
+                settings.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36";
+                settings.CachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\CEF";
+                settings.UserDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\CEF\User data\";
+                settings.PersistSessionCookies = true;
+                settings.PersistUserPreferences = true;
+                Cef.Initialize(settings);
+            }
+                
+        }
+
+        private void resizeControls(int h, int w)
+        {
+            if (currentWindow == "")
+                return;
+            btnPanel.Height = h;
+            mPanel.Height = h;
+            mPanel.Width = w - 70;
+            mPanel.Left = 70;
+            mPanel.Controls.Add(dict[currentWindow]);
+        }
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            Control control = sender as Control;
+            resizeControls(ClientRectangle.Height, ClientRectangle.Width);
+
+        }
+
+        private void readconsolemessage(object sender, CefSharp.ConsoleMessageEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("New console message:");
+            System.Diagnostics.Debug.WriteLine(e.Message);
+        }
+        
+        private ChromiumWebBrowser newBrowser(string url)
+        {
+            // Turn the url into something useable
+            Uri uri = new System.Uri(url, System.UriKind.Absolute);
+
+            // Create a new empty browser
+            ChromiumWebBrowser wb = new ChromiumWebBrowser("");
+            
+            // Load the url
+            wb.Load(url);
+
+            // Same dimensions as the panel
+            wb.Top = mPanel.Top;
+            wb.Left = mPanel.Left;
+
+            // Start hidden
+            wb.Visible = false;
+            
+            // Prints browser console messages to the  console
+            wb.ConsoleMessage += readconsolemessage;
+
+            // Handles popups and new tabs/new windows
+            wb.LifeSpanHandler = new BrowserLifeSpanHandler();
+            return wb;
+        }
+
+        private Button NewButton(string name, int top)
+        {
+            Button db = new Button();
+
+            // Set Button properties
+            db.Location = new Point(0, 0);
+            db.Top = top;
+            db.Left = 10;
+            db.Width = 50;
+            db.Height = 50;
+            db.FlatStyle = FlatStyle.Flat;
+            db.FlatAppearance.BorderSize = 0;
+            db.FlatAppearance.BorderColor = Color.Red;
+            db.Name = name;
+            db.Font = new Font("Georgia", 16);
+            db.Click += new EventHandler( BtnClick );
+            db.MouseEnter += BtnEnter;
+            db.MouseLeave += BtnLeave;
+
+
+
+            // Add Button to the Form. Placement of the Button
+
+            // will be based on the Location and Size of button
+
+
+
+            return db;
+        }
+        void BtnClick(object sender, EventArgs e)
+        {
+            if (currentWindow != "")
+            {
+                dict[currentWindow].Visible = false;
+                dict[currentWindow].SendToBack();
+                mPanel.Controls.Remove(dict[currentWindow]);
+            }
+
+            Button btn = sender as Button;
+            dict[btn.Name].Visible = true;
+            dict[btn.Name].BringToFront();
+            currentWindow = btn.Name;
+
+            resizeControls(ClientRectangle.Height, ClientRectangle.Width);
+        }
+        void BtnEnter(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.FlatAppearance.BorderSize = 1;
+        }
+        void BtnLeave(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.FlatAppearance.BorderSize = 0;
+        }
+    }
+
+    /*
+     * This class handles popups
+     */
+    public class BrowserLifeSpanHandler : ILifeSpanHandler
+    {
+        //http://cefsharp.github.io/api/57.0.0/html/T_CefSharp_WindowOpenDisposition.htm
+        // The types of popups we want to load, all others will be cancelled
+        int[] allowedDispositions = { 1, 2, 3, 4};
+
+        public bool OnBeforePopup(IWebBrowser browserControl, IBrowser browser, IFrame frame, string targetUrl, string targetFrameName,
+            WindowOpenDisposition targetDisposition, bool userGesture, IPopupFeatures popupFeatures, IWindowInfo windowInfo,
+            IBrowserSettings browserSettings, ref bool noJavascriptAccess, out IWebBrowser newBrowser)
+        {
+            newBrowser = null;
+            if (allowedDispositions.Contains((int)targetDisposition))
+            {
+                browserControl.Load(targetUrl);   
+            }
+            return true;
+        }
+
+        public void OnAfterCreated(IWebBrowser browserControl, IBrowser browser)
+        {
+            //
+        }
+
+        public bool DoClose(IWebBrowser browserControl, IBrowser browser)
+        {
+            return false;
+        }
+
+        public void OnBeforeClose(IWebBrowser browserControl, IBrowser browser)
+        {
+            //nothing
+        }
+    }
+}
